fc83afc0988f9534a1c1285ceaceda11ceb6dc35
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2023-05-16 07:37:06 +0300

    - Merged classes into one - Corrected the display of information

--- ClassExample01.java
+++ ClassExample01.java
@@ -1,4 +1,33 @@
 import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 
-public abstract class ClassExample01 implements Serializable, Cloneable {
+public class ClassExample01 implements Serializable, Cloneable {
+    public static int publicStaticIntField = 1;
+    protected static int staticIntField = 2;
+    public static String publicStaticStringField = "publicStaticStringField";
+    private static String staticStringField = "privateStaticStringField";
+
+    public ClassExample01(int id) throws ClassCastException, ArithmeticException {
+    }
+
+    public ClassExample01(int id, String name) throws RemoteException, SQLException {
+    }
+
+    public ClassExample01(int id, String name, String address) throws InterruptedException {
+    }
+
+    public void add(int firstElement, int secondElement, String result) throws ClassNotFoundException, ClassCastException {
+        System.out.println("Add - demo method for Reflection API");
+    }
+
+    public String search(String searchString) throws ArithmeticException, InterruptedException {
+        System.out.println("Search - demo method for Reflection API");
+
+        return null;
+    }
+
+    public void delete(String deleteString) throws SQLException {
+        System.out.println("Delete - demo method for Reflection API");
+    }
 }
